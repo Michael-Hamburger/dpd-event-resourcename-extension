11017695292f30263d429ddf08fa2974103c6bfc
Michael <michael.hamburger@mail.de>
2016-10-17 11:54:34 +0200

    Create event-extension.js

---
+++ event-extension.js
@@ -0,0 +1,16 @@
+(function(Script) {
+  var _run = Script.prototype.run;
+  Script.prototype.run = function (ctx, domain, fn)
+  {
+    const path = require('path');
+    let resourcePath = this.path;
+    if (typeof domain === 'object') {
+      domain.resourceName = function () // access to the current resourceName the current event is running
+      { 
+          var pathSegments = path.dirname(resourcePath).split(path.sep);
+          return pathSegments[pathSegments.length - 1];
+      };
+    }
+    _run.call(this, ctx, domain, fn);
+  }
+})(require('deployd/lib/script'));
